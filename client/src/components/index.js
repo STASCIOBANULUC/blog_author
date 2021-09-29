8dee6e62e3f028c78d04710b90a3870e83ae1b56
Игорь Чехунов <igorcehunov@MacBook-Pro-Igor.local>
2021-09-30 01:25:50 +0300

    first com

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Header } from './Header';
+export { default as Top } from './Top';
+export { default as Best } from './Best';
+export { default as Menu } from './Menu';
+export { default as Site } from './Site';
+export { default as Preloader } from './Preloader';
+export { default as About } from './About';
+export { default as Pagination } from './pagination';
