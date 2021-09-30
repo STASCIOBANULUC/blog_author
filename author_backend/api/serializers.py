013ee81d9228b084f8bfd018fba0ab155cf495d7
Stas <stasciobanu777@gmail.com>
2021-09-30 15:07:52 +0300

    fix

--- author_backend/api/serializers.py
+++ author_backend/api/serializers.py
@@ -6,10 +6,10 @@ from author_backend.models import Category, Verse
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
-        fields = ['__all__']
+        fields = ['title', 'slug']
 
 
 class VerseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Verse
-        fields = ['__all__']
+        fields = ['category', 'title', 'text', 'consecrated', 'date_add', 'publish', 'slug']
